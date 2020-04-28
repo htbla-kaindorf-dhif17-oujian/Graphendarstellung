aed6cd5cfccbfccc28978445047cc28c07f2fde8
Gottl <Gottl@DESKTOP-OK02R3D>
2020-04-28 10:46:06 +0200

    implemented basic methods

--- src/bl/Main.java
+++ src/bl/Main.java
@@ -5,10 +5,47 @@
  */
 package bl;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Gottl
  */
 public class Main {
     
+    private char[] knotenListe;
+    private int[][] matrix;
+    
+    public void ladeDaten() throws IOException{
+        
+        BufferedReader br = new BufferedReader(new FileReader(Paths.get(System.getProperty("user.dir"), "src","res","daten.csv").toFile()));
+        
+    }
+    
+    public boolean existiertKante(char startKnoten, char endKnoten){
+        return false;
+    }
+    
+    public char[] getAlleNachbarn(char knoten){
+        return null;
+    }
+    
+    public boolean existiertKnoten(char knoten){
+        for (char c : knotenListe) {
+            if(knoten == c){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
