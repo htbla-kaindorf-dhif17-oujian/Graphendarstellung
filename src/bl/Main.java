18fff0a7539683a4bcba64ea2c216f6bf154dbfa
Gottl <Gottl@DESKTOP-OK02R3D>
2020-04-28 11:43:11 +0200

    implemented UI auto formatting by netbeans

--- src/bl/Main.java
+++ src/bl/Main.java
@@ -11,7 +11,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,99 +22,155 @@ import java.util.logging.Logger;
  * @author Gottl
  */
 public class Main {
-    
+
     private char[] knotenListe;
     private int[][] matrix;
-    
-    public void ladeDaten() throws IOException{
-        
-        BufferedReader br = new BufferedReader(new FileReader(Paths.get(System.getProperty("user.dir"), "src","res","daten.csv").toFile()));
+
+    public void ladeDaten() throws IOException {
+
+        BufferedReader br = new BufferedReader(new FileReader(Paths.get(System.getProperty("user.dir"), "src", "res", "daten.csv").toFile()));
         String line = br.readLine();
         String[] lineParts = line.split(",");
         knotenListe = new char[lineParts.length - 1];
-        for(int i = 1; i<lineParts.length; i++){
-            knotenListe[i-1] = lineParts[i].charAt(0);
+        for (int i = 1; i < lineParts.length; i++) {
+            knotenListe[i - 1] = lineParts[i].charAt(0);
         }
         matrix = new int[knotenListe.length][knotenListe.length];
-        
-        for(int i = 0;i < knotenListe.length; i++){
+
+        for (int i = 0; i < knotenListe.length; i++) {
             line = br.readLine();
             lineParts = line.split(",");
-            for(int j = 1; j < lineParts.length;j++){
-                matrix[i][j-1] = Integer.parseInt(lineParts[j]);
+            for (int j = 1; j < lineParts.length; j++) {
+                matrix[i][j - 1] = Integer.parseInt(lineParts[j]);
             }
         }
-        
+
     }
-    
-    public boolean existiertKante(char startKnoten, char endKnoten){
+
+    public boolean existiertKante(char startKnoten, char endKnoten) {
         int index1 = getIndex(startKnoten);
         int index2 = getIndex(endKnoten);
-        if(matrix[index1][index2] == 1){
+        if (matrix[index1][index2] == 1) {
             return true;
         }
         return false;
     }
-    
-    public char[] getAlleNachbarn(char knoten){
+
+    public char[] getAlleNachbarn(char knoten) {
         int knotenIndex = getIndex(knoten);
         List<Character> nachbarn = new ArrayList<>();
-        for(int i = 0; i< matrix[knotenIndex].length; i++){
-            if(matrix[knotenIndex][i] == 1){
+        for (int i = 0; i < matrix[knotenIndex].length; i++) {
+            if (matrix[knotenIndex][i] == 1) {
                 nachbarn.add(knotenListe[i]);
             }
         }
         char[] nachbarnFeld = new char[nachbarn.size()];
-        for(int i = 0; i< nachbarn.size(); i++){
-            nachbarnFeld[i] = nachbarn.get(i);
+        if (nachbarn.isEmpty()) {
+            return null;
+        } else {
+            for (int i = 0; i < nachbarn.size(); i++) {
+                nachbarnFeld[i] = nachbarn.get(i);
+            }
+
+            return nachbarnFeld;
         }
-        return nachbarnFeld;
     }
-    
-    public int getIndex(char knoten){
-        
-        for(int j = 0; j < knotenListe.length; j++){
-            if(knotenListe[j]==knoten){
+
+    public int getIndex(char knoten) {
+
+        for (int j = 0; j < knotenListe.length; j++) {
+            if (knotenListe[j] == knoten) {
                 return j;
             }
         }
-        throw new RuntimeException("Knoten "+ knoten + " existiert nicht");
+        throw new RuntimeException("Knoten " + knoten + " existiert nicht");
     }
-    
-    public boolean existiertKnoten(char knoten){
+
+    public boolean existiertKnoten(char knoten) {
         for (char c : knotenListe) {
-            if(knoten == c){
+            if (knoten == c) {
                 return true;
             }
         }
         return false;
     }
-    
-    public String toString(){
+
+    public String toString() {
         String output = "  ";
         for (char c : knotenListe) {
             output += c + " ";
         }
         output += "\n-----------\n";
-        for(int i = 0; i<knotenListe.length; i++){
+        for (int i = 0; i < knotenListe.length; i++) {
             output += knotenListe[i] + " ";
-            for(int j = 0; j< matrix[i].length;j++){
+            for (int j = 0; j < matrix[i].length; j++) {
                 output += matrix[i][j] + " ";
             }
             output += "\n";
         }
         return output;
     }
-    
+
     public static void main(String[] args) {
         try {
             Main main = new Main();
             main.ladeDaten();
-            System.out.println(main.toString());
-            System.out.println(main.existiertKnoten('B'));
-            System.out.println(main.existiertKante('B', 'C'));
-            System.out.println(main.existiertKante('D', 'C'));
-            System.out.println(main.getAlleNachbarn('C'));
+            Scanner scan = new Scanner(System.in);
+            OUTER:
+
+            do {
+
+                System.out.println(
+                        "Bitte eine Option auswaehlen:\n"
+                        + "(1) Ausgeben der Matrix\n"
+                        + "(2) Pruefen ob ein Knoten existiert\n"
+                        + "(3) Pruefen ob eine Kante existiert\n"
+                        + "(4) Alle Nachbarn eines Knoten ausgeben\n"
+                        + "(5) Beenden des Programms\n"
+                        + "Bitte Option auswaehlen: ");
+                int input = scan.nextInt();
+                char knoten = '_';
+                switch (input) {
+                    case 1:
+                        System.out.println(main.toString());
+                        break;
+                    case 2:
+                        System.out.print("Bitte einen Knoten eingeben: ");
+                        knoten = scan.next().charAt(0);
+                        if (main.existiertKnoten(knoten)) {
+                            System.out.println("Knoten " + knoten + " existiert");
+                        } else {
+                            System.out.println("Knoten " + knoten + " existiert nicht");
+                        }
+                        break;
+                    case 3:
+                        System.out.print("Bitte Knoten 1 eingeben: ");
+                        char knoten1 = scan.next().charAt(0);
+                        System.out.print("Bitte Knoten 2 eingeben: ");
+                        char knoten2 = scan.next().charAt(0);
+                        if (main.existiertKante(knoten1, knoten2)) {
+                            System.out.format("Kante zwischen Knoten %c und %c existiert\n", knoten1, knoten2);
+                        } else {
+                            System.out.format("Kante zwischen Knoten %c und %c existiert nicht\n", knoten1, knoten2);
+                        }
+                        break;
+                    case 4:
+                        System.out.print("Bitte Knoten eingeben: ");
+                        knoten = scan.next().charAt(0);
+                        char[] nachbarn = main.getAlleNachbarn(knoten);
+                        if (nachbarn == null) {
+                            System.out.println("Knoten " + knoten + " hat keine Nachbarn");
+                        } else {
+                            System.out.println("Nachbarn von Knoten " + knoten + " sind " + Arrays.toString(main.getAlleNachbarn(knoten)));
+                        }
+                        break;
+                    case 5:
+                        break OUTER;
+                    default:
+                        System.out.println("Bitte eine Zahl zwischen 1 und 5 eingeben");
+                        break;
+                }
+            } while (true);
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
